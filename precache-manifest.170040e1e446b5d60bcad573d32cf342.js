ca229d66c4df3ce7632d6d619d85274ba713037b
Christopher Klapp <christopher@klapp.name>
2019-03-04 20:44:47 -0500

    Updates

---
+++ precache-manifest.170040e1e446b5d60bcad573d32cf342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2ac1b0be50334f82891",
+    "url": "/translation-process/static/css/main.91b403ba.chunk.css"
+  },
+  {
+    "revision": "c2ac1b0be50334f82891",
+    "url": "/translation-process/static/js/main.c2ac1b0b.chunk.js"
+  },
+  {
+    "revision": "f782d2c7352b73383913",
+    "url": "/translation-process/static/js/runtime~main.f782d2c7.js"
+  },
+  {
+    "revision": "f747e71b43c736a4af24",
+    "url": "/translation-process/static/js/2.f747e71b.chunk.js"
+  },
+  {
+    "revision": "f9e6e01238a5c65a0019e0d1485bfa6b",
+    "url": "/translation-process/index.html"
+  }
+];
